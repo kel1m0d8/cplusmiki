aa484a911049e057ed7933d40ef99f8c2d5e67f1
kel1m0d8 <kel1m0d8>
2025-12-21 23:19:31 -0500

    Week 1: update hello output message

--- week01/hello.cpp
+++ week01/hello.cpp
@@ -2,6 +2,7 @@
 
 int main(){
 	std::cout << "C++ fundamentals journey started." << std::endl;
-	std::cout << "Week 1: Hello C++ world." << std::endl;
+	std::cout << "Week 1: Hello from C++ world(compiled binary)." << std::endl;
+	
 	return 0;
 }
